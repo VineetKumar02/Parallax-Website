39223bb27c47c939531ecddd4aa2747a60a0f4b2
VineetKumar02 <vineethero02@gmail.com>
2023-04-30 12:47:33 +0530

    Initial commit

---
+++ action.js
@@ -0,0 +1,85 @@
+// Select the class bubble
+time = document.getElementsByClassName('bubbles')[0];
+
+// padding values for desktop
+var fish2move = 100;
+var fish3move = 900;
+var fish4move = 1200;
+
+if (screen.width < 400) {
+
+    //Change transformation duration and translatey for mobile view
+    time.style.setProperty('--transform-duration', '15s')
+    time.style.setProperty('--transform-y', '-700vh')
+
+    // padding values for mobile
+    fish2move = 1680;
+    fish3move = 3000;
+    fish4move = 4300;
+}
+
+
+
+window.addEventListener('scroll', function () {
+
+    let value = window.scrollY;   //Get Scroll Value (Mobile - High)
+
+    text.style.top = 50 + value * -0.2 + '%';
+    cloud.style.left = value * 2 + 'px';
+
+    bird1.style.top = value * 0.1 + 'px';
+    bird1.style.left = value * 1 + 'px';
+
+    bird2.style.top = value * -0.1 + 'px';
+    bird2.style.left = value * -2 + 'px';
+
+    explore.style.marginTop = value * 1.5 + 'px';
+
+    rocks.style.top = value * -0.14 + 'px';
+
+    forest.style.top = value * 0.4 + 'px';
+    sky.style.top = value * 0.25 + 'px';
+    mountains.style.top = value * 0.25 + 'px';
+
+    header.style.top = value * 0.7 + 'px';
+    sun.style.top = value * 1 + 'px';
+
+    //To prevent splash to move above sea water
+    if (value < 400) {
+        splash.style.top = 20 + value * -0.3 + 'px';
+    }
+
+    //Move fishes horizontally
+    fish1.style.right = (value - 100) * 1 + 'px';
+    fish2.style.left = (value - fish2move) * 1 + 'px';
+    fish3.style.right = (value - fish3move) * 1 + 'px';
+    fish4.style.left = (value - fish4move) * 1 + 'px';
+})
+
+
+//Links for social media icons in footer
+var links = document.getElementsByClassName("button");
+
+links[0].addEventListener("click", () => { openlink(1) });
+links[1].addEventListener("click", () => { openlink(2) });
+links[2].addEventListener("click", () => { openlink(3) });
+links[3].addEventListener("click", () => { openlink(4) });
+links[4].addEventListener("click", () => { openlink(5) });
+
+function openlink(x) {
+    if (x == 1) {
+        window.open("https://www.facebook.com/VineetKumar02/", "_blank");
+    }
+    if (x == 2) {
+        window.open("https://twitter.com/Vineethero02", "_blank");
+    }
+    if (x == 3) {
+        window.open("https://www.instagram.com/_.vini._02_/", "_blank");
+    }
+    if (x == 4) {
+        window.open("https://www.linkedin.com/in/vineet-kumar-gupta-2833ab196/", "_blank");
+    }
+    if (x == 5) {
+        window.open("https://github.com/VineetKumar02", "_blank");
+    }
+}
